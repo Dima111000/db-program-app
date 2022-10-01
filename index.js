d13a79c5acc5cd9d092b0233f2a2e8dffe2c5b9b
Dima111000 <dimafuga2003@gmail.com>
2022-10-01 15:34:35 +0300

    new add

--- index.js
+++ index.js
@@ -7,28 +7,28 @@ const app = express()
 app.use(parser.urlencoded({extended: true}))
 const port = process.env.PORT || 5000
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
-})
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialect: 'postgres',
+//     protocol: 'postgres',
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false
+//         }
+//     }
+// })
 
-const connectDb = async() => {
-    console.log('Проверка запуска БД...')
-
-    try {
-        await sequelize.authenticate();
-        console.log('БД успешно подключена')
-    } catch(e) {
-        console.log('Ощибка подключения БД', e)
-        process.exit(1)
-    }
-}
+// const connectDb = async() => {
+//     console.log('Проверка запуска БД...')
+//
+//     try {
+//         await sequelize.authenticate();
+//         console.log('БД успешно подключена')
+//     } catch(e) {
+//         console.log('Ощибка подключения БД', e)
+//         process.exit(1)
+//     }
+// }
 
 app.get('/', (req, res) => {
      res.sendFile(__dirname+'/index.html');
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
 
 
 ;(async () => {
-    await connectDb();
+    // await connectDb();
 
     console.log(`Начало запуска сервера на порту ${port}...`);
 
