7822797636eebb390b45b620fc3a5114d36d4a27
Dima111000 <dimafuga2003@gmail.com>
2022-10-01 14:19:50 +0300

    new add

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+const express = require('express')
+const parser = require("body-parser")
+const { Sequelize } = require('sequelize')
+
+const app = express()
+app.use(parser.urlencoded({extended: true}))
+const port = process.env.PORT || 5000
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+})
+
+const connectDb = async() => {
+    console.log('Проверка запуска БД...')
+
+    try {
+        await sequelize.authenticate();
+        console.log('БД успешно подключена')
+    } catch(e) {
+        console.log('Ощибка подключения БД', e)
+        process.exit(1)
+    }
+}
+
+app.get('/', (req, res) => {
+     res.sendFile(__dirname+'/index.html');
+})
+
+
+;(async () => {
+    await connectDb();
+
+    console.log(`Начало запуска сервера на порту ${port}...`);
+
+    app.listen(port, () => {
+        console.log(`Сервер запущен на порту ${port}`);
+    });
+})();
+
+
